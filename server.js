1f3952425b372f8b53be7e6c98afe45812d84a44
Diego Angulo <ancol.productividad@gmail.com>
2025-07-29 22:10:24 -0500

    Versión inicial del backend cargodesk

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const authRoutes = require('./routes/auth.routes');
+const dataRoutes = require('./routes/data.routes');
+const uploadRoutes = require('./routes/upload.routes');
+const auctionsRoutes = require('./routes/auctions.routes');
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/data', dataRoutes);
+app.use('/api/upload', uploadRoutes);
+app.use('/auctions', auctionsRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor backend corriendo en puerto ${PORT}`);
+});
